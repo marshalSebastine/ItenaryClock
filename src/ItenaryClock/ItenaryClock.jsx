756becfa65bc5cc8b6eff08a9c32ad5444d42d3d
marshal <marshalalakkadan@gmail.com>
2023-08-08 07:12:20 +0530

    placing divs around the itenary clock done.

--- src/ItenaryClock/ItenaryClock.jsx
+++ src/ItenaryClock/ItenaryClock.jsx
@@ -1,9 +1,16 @@
 import './ItenaryClock.styles.css';
 
 const ItenaryClock = () => {
+    let numberofchildren = 3
+    let placeAtAngleGap = 360/numberofchildren
+    let childrenDummyList = ['child1','child2','child3']
+    let clockradius = 450/2
     return(
         <div className='itenaryclock_wrapper'>
-
+            {childrenDummyList.map((child,index) => {
+               let transformstyle =  {transform: `rotate(${index*placeAtAngleGap}deg) translate(calc(var(--circlerad)*-1px)) rotate(${index*placeAtAngleGap*-1}deg)`}
+               return <div style={transformstyle} className='timerunners'/>
+            })}           
         </div>
     )
 }
